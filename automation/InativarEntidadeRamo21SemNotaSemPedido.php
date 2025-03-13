62c68eac14d4ff44d891a69cb7d9199a8fbde88c
Vini <ti03@NOTE-LM-0012.liottomarcon.ind.br>
2025-03-13 09:37:00 -0300

    automacao inativar entidade com ramo de atividade 21 quando nao tiver nota em 6 meses e nao tiver pedido pendente

---
+++ automation/InativarEntidadeRamo21SemNotaSemPedido.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once __DIR__ . "/../config/init.php";
+
+// A ideia foi solicitada pela Rebeca/Michele do Comercial, Chamado #10607
+// Filtrar clientes com o ramo de atividade 21 que não tiveram nenhuma nota entre 6 meses e não possuem nenhum pedido pendente.
+// Aos que entrarem na lógica, desativar e alterar a data de próxima analise para a data que foi atualizado.
+
+class InativarEntidadeRamo21SemNotaSemPedido extends DB {
+    public function __construct() {
+        parent::__construct();
+        $datas = $this->conn(self::getCodcli());
+        foreach ($datas as $data) {
+            $dataAtualiza = self::atualizaDataPrevAnaliseEntidade($data['CODCLI']);
+            $this->conn($dataAtualiza);
+        }
+    }
+    function logError($message, $dados = null) {
+        $errorFile = __DIR__ . '/../errors/automation/InativarEntidadeRamo21SemNotaSemPedido/error-' . date('Y-m-d') . '.log';
+        $file = fopen($errorFile, 'a+');
+        fwrite($file, "Erro: $message\n");
+        if ($dados) {
+            fwrite($file, "Dados: " . json_encode($dados, JSON_UNESCAPED_UNICODE) . "\n");
+        }
+        fclose($file);
+    }
+    private static function getCodcli () {
+        return "SELECT DISTINCT e.codcli FROM ENTIDADE_001 e
+            WHERE e.SIT_CLI = '21'
+            AND e.CODCLI NOT IN (SELECT DISTINCT n.codcli FROM NOTA_002 n WHERE n.DT_EMISSAO BETWEEN (CURRENT_DATE - (30*6)) AND CURRENT_DATE)
+            and e.codcli not in (SELECT DISTINCT p.CODCLI FROM PEDIDO_001 p INNER JOIN PED_ITEN_001 pi2 ON pi2.NUMERO = p.NUMERO WHERE pi2.qtde > 0 AND p.nota is NULL)
+        ";
+    }
+    private static function atualizaDataPrevAnaliseEntidade($id) {
+        $dataCorrente = date("Y-m-d");
+        return "UPDATE ENTIDADE_001 SET dt_prev_analise = '$dataCorrente', ativo = 'N' where codcli = '$id'";
+    }
+    private function conn($sql) {
+        try {
+            $stmt = $this->getConn()->prepare($sql);
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $data;
+        } catch (PDOException $e) {
+            $this->logError('Erro no sql ou conexão com Banco de dados', $e->getMessage());
+        } catch (Exception $err) {
+            $this->logError('Erro no servidor ou com programa com erro', $err->getMessage());
+
+        }
+    }
+}
+
+
+new InativarEntidadeRamo21SemNotaSemPedido();
