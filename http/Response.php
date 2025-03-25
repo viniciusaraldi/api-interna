589c22150b361c897ff243b7a89a471c8f4898dd
Vinicius - ti03 <ti03@liottomarcon.ind.br>
2025-03-25 15:44:02 -0300

    ajuste servidor

--- http/Response.php
+++ http/Response.php
@@ -2,7 +2,7 @@
 
 class Response {
 
-    public static function json(array $data, int|string $status = 200) {
+    public static function json(array $data, int $status = 200) {
         http_response_code($status);
 
         return $data;
