75bef46ac32ded0d008234f91473c37b9ed143af
Vini <ti03@NOTE-LM-0012.liottomarcon.ind.br>
2025-04-23 07:18:02 -0300

    novo metodo implementado, nova acao implementada

--- controller/Estilo.php
+++ controller/Estilo.php
@@ -7,13 +7,19 @@ class Estilo extends DB {
 
     public static $pathfile = "/../public/fotos/amostras/";
     public static $destPathFile;
-    private $tableDb = 'TABF08';
+    private $tableDb = 'TABF07';
     
     public function __construct() {
         parent::__construct();
         self::$destPathFile = parent::getPathEstilo();
     }
 
+    /**
+     * Função para tratar comando de SQL, column significa uma coluna especifica (se selecionado é necessário colocar colocar allData 1), allData siginifica todas as colunas (necessário deixar o column entre aspas sem conteudo dentro, ou seja, '');
+     * @param string $sql
+     * @param string $column
+     * @param bool $allData
+     */
     private function getSql($sql, $column = '', $allData = 0) {
         $stmt = $this->getConn()->prepare($sql);
         $stmt->execute();
@@ -23,22 +29,30 @@ class Estilo extends DB {
                 'messagem' => "Houve algum problema ao preparar o comando",
                 'sql' => $sql,
             ], 403);
-            exit;
         }
 
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // if (empty($result)) {
-        //     return Response::json([
-        //         'success' => false,
-        //         'messagem' => "Nenhum resultado encontrado para a query",
-        //         'sql' => $sql,
-        //     ], 404);
-        // }
         
         return $allData == 0 ? (
             $column == '' ? $result[0] : $result[0][strtoupper($column)]
             ) : ['colunas' => $result, 'linhas' => $stmt->rowCount()];
     }
+
+    /**
+     * Função para incluir na tabela
+     * @param mixed $id
+     * @param string $amostra
+     * @param string $prototipo
+     * @param string $pontosMedicao
+     * @param mixed $tolerancia
+     * @param mixed $toleranciaMin
+     * @param mixed $toleranciaMax
+     * @param string $tamanhos
+     * @param string $valorTamanhos
+     * @param bool $status
+     * @param string $valorTamanhosReal
+     * @return array|array{amostra: mixed, id: mixed, maxima: mixed, minima: mixed, ponto_medicao: mixed, prototipo: mixed, rowsAffected: mixed, status: mixed, tamanhos: mixed, tolerancia: mixed, valorTamanhos: mixed, valorTamanhosReal: mixed}
+     */
     private function insertAmostra($id, $amostra, $prototipo, $pontosMedicao, $tolerancia, $toleranciaMin, $toleranciaMax, $tamanhos, $valorTamanhos, $status = 0, $valorTamanhosReal) {
         try {
             $data = $this->getSql("INSERT INTO $this->tableDb (id,amostra, prototipo, pontos_Medicao, tolerancia, tolerancia_min, tolerancia_max, tamanhos, valor_tamanhos, status, valor_tamanhos_real) VALUES 
@@ -64,27 +78,53 @@ class Estilo extends DB {
             ], 500);
         }
     }
+
+    /**
+     * Função para verificar se existe ou não as colunas selecionadas
+     * @param array $data
+     * @return bool
+     */
     private static function isSetAmostraDatas(array $data) {
-        return isset($data['prototipo'],$data['pontos_medicao'],$data['tolerancia'], $data['tolerancia_min'], $data['tolerancia_max'], $data['tamanhos'], $data['valor_tamanhos']);
+        return isset($data['prototipo'],$data['pontos_medicao'],$data['tolerancia'], $data['tolerancia_min'], $data['tolerancia_max'], $data['tamanhos'], $data['valor_tamanhos'],$data['valor_tamanhos_real']);
     }
+
+    /**
+     * Função para verificar se existe o caminho das imagens das amostras do prototipoa
+     * @param string $path
+     * @param string $prototipo
+     * @param string $amostra
+     * @return void
+     */
     private static function existsPathIamgeAmostra($path, $prototipo, $amostra) {
         $fullPath = str_replace("\\", '/', realpath(__DIR__ . "/../public")) . $path . $prototipo . "/";
 
         if (file_exists($fullPath)) {
             if (file_exists($fullPath . "/$amostra")) {
-                return true;
+                return;
             } else {
                 mkdir($fullPath . "/$amostra", 0777, true);
-                return true;
+                return;
             }
         } else {
             mkdir($fullPath, 0777, true);
-            $amostra = mkdir($fullPath . $amostra, 0777, true);
+            mkdir($fullPath . $amostra, 0777, true);
+            return;
         }
     }
+
+    /**
+     * Verifica se existe o prototipo
+     * @param string $prototipo
+     * @return bool
+     */
     private function getPrototipo(string $prototipo) {
         return $this->getSql("SELECT * FROM PRODUTO_001 WHERE PROTOTIPO = '$prototipo'", 'prototipo', 0) == '' ? false : true;
     }
+
+    /**
+     * Summary of get
+     * @param array $path
+     */
     public function get(array $path) {
         try {
             $props = [
@@ -125,6 +165,11 @@ class Estilo extends DB {
             ], 500);
         }
     }
+
+    /**
+     * Summary of post
+     * @param array $path
+     */
     public function post(array $path) {
         try {
             $props = [
@@ -147,7 +192,12 @@ class Estilo extends DB {
                 'messagem' => "Error: " . $e->getMessage(),
             ], 500);
         }
-    } 
+    }
+
+    /**
+     * Summary of put
+     * @param array $path
+     */
     public function put(array $path) {
         try {
             $props = [
@@ -167,10 +217,45 @@ class Estilo extends DB {
                 'messagem' => "Error: " . $e->getMessage(),
             ], 500);
         }
-    } 
+    }
+    
+    /**
+     * Summary of delete
+     * @param array $path
+     */
+    public function delete(array $path) {
+        try{ 
+            $props = [
+                'deleteAmostra'
+            ];
+            $funcao = $path[0];
+            $body = $path[2];
+
+            return in_array($funcao, $props) ? $this->{$funcao}($body) : Response::json([
+                'success' => false,
+                'messagem' => 'Não foi encontrado a rota ' . $funcao,
+            ], 403);
+        } catch (Exception $e) {
+            return Response::json([
+                'success' => false,
+                'messagem' => "Error: " . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Retorna o prototipo, e as amostras de mesmo prototipo
+     * @return array
+     */
     private function getPrototipoPorAmostra () {
         return $this->getSql("SELECT  DISTINCT PROTOTIPO, list(amostra) AS amostra FROM (SELECT distinct prototipo, amostra FROM $this->tableDb order by prototipo, amostra) GROUP BY prototipo", '', 1);
     }
+
+    /**
+     * Busca as Imagens do prototipo e da amostra
+     * @param array $data
+     * @return array
+     */
     private function getImagesAmostras(array $data = []) {
         try {
             list($amostra, $prototipo) = isset($data['amostra'], $data['prototipo']) ? [$data['amostra'],$data['prototipo']] : [null,null];
@@ -205,6 +290,12 @@ class Estilo extends DB {
             ], 500);
         }
     }
+
+    /**
+     * Busca as amostras do prototipo, com/sem filtrar pela amostra
+     * @param array $data
+     * @return array
+     */
     private function getAmostra(array $data = []) {
         try {
             $sql = '';
@@ -234,6 +325,12 @@ class Estilo extends DB {
             ], 500);
         }
     }
+
+    /**
+     * Busca a faixa do prototipo
+     * @param array $amostra
+     * @return mixed
+     */
     private function getFaixaPrototipo(array $amostra) {
         if (!isset($amostra['prototipo'])) {
             return Response::json([
@@ -246,6 +343,12 @@ class Estilo extends DB {
         $data = $this->getSql("SELECT  DISTINCT '$prototipo' as prototipo, f.faixa, f.posicao, f.tamanho FROM FAIXA_ITEN_001 f WHERE f.FAIXA IN (SELECT faixa FROM produto_001 p WHERE p.PROTOTIPO = '$prototipo')", '', 1);
         return isset($data['colunhas']) ? $data['colunas'] : $data;
     }
+
+    /**
+     * Seta status (reprovado para 0 e aprovado como 1) na amostra, ao aprovado será incluido no banco
+     * @param mixed $data
+     * @return array
+     */
     private function setStatus ($data) {
         try {
             $prototipo = $this->getPrototipo($data['prototipo']) ? $data['prototipo'] : 0;
@@ -317,6 +420,12 @@ class Estilo extends DB {
             ], 500);
         }
     }
+
+    /**
+     * Inclui o teste de uso para cada amostra e para cada prototipo
+     * @param mixed $data
+     * @return array
+     */
     private function setTesteUso ($data) {
         try {
             $prototipo = $this->getPrototipo($data['prototipo']) ? $data['prototipo'] : 0;
@@ -345,6 +454,13 @@ class Estilo extends DB {
             ], 500);
         }
     }
+
+    /**
+     * Inclui imagens e/ou a observação da amostra
+     * @param array $data
+     * @param array $files
+     * @return array
+     */
     private function setFotosAmostra($data, $files = null) {
         try {
             $obsAmostraRequesition = $data['obs_amostra'] ?? '';
@@ -421,6 +537,12 @@ class Estilo extends DB {
             ], 500);
         }
     }
+
+    /**
+     * Inclui a amostra na tabela no banco
+     * @param array $datas
+     * @return array
+     */
     private function setAmostra(array $datas) {
         try {
             $prototipo = $this->getPrototipo($datas[0]['prototipo']) ? $datas[0]['prototipo'] : 0;
@@ -445,7 +567,8 @@ class Estilo extends DB {
                                 'tolerancia_min' => $data['tolerancia_min'],
                                 'tolerancia_max' => $data['tolerancia_max'],
                                 'tamanhos' => $data['tamanhos'],
-                                'valor_tamanhos' => $data['valor_tamanhos'], 
+                                'valor_tamanhos' => $data['valor_tamanhos'],
+                                'valor_tamanhos_real' => $data['valor_tamanhos_real'],
                                 ]
                         ], 403);
                     } else {
@@ -469,6 +592,12 @@ class Estilo extends DB {
             ], 500);
         }
     }
+
+    /**
+     * Edita os dados da amostra já incluida na banco
+     * @param array $data
+     * @return array
+     */
     private function editPontosMedicao(array $data) {
         try {
             $prototipo = $this->getPrototipo(isset($data['prototipo']) ? $data['prototipo'] : 0) ? $data['prototipo'] : 0;
@@ -512,4 +641,53 @@ class Estilo extends DB {
             ], 500);
         }
     }
+
+    /**
+     * Exclui a amostra no banco e as imagens gravadas na pasta
+     * @param array $datas
+     * @return array
+     */
+    private function deleteAmostra(array $datas) {
+        try {
+            $prototitpo = $this->getAmostra(['prototipo' => isset($datas['prototipo']) ? $datas['prototipo'] : '', 'amostra' => isset($datas['amostra']) ? $datas['amostra'] : '']);
+
+            if ($prototitpo && $prototitpo['messagem']['linhas'] > 0) {
+                // glob = Retorna os arquivos ou pastas dentro do path informado.
+                $amostras = glob(__DIR__ . self::$pathfile . $datas['prototipo'] . '/' . $datas['amostra'] . '/*');
+                foreach ($amostras as $value) {
+                    unlink($value);
+                }
+                rmdir(__DIR__ . self::$pathfile . $datas['prototipo'] . '/' . $datas['amostra']);
+                
+                
+                $sql = "DELETE FROM $this->tableDb where prototipo = '{$datas['prototipo']}' and amostra = '{$datas['amostra']}' ";
+                $prototipoExcluido = $this->getSql($sql);
+                 
+                return Response::json([
+                    'message' => 'Deletado prototipo com sucesso!',
+                    'prototipo' => $datas['prototipo'],
+                    'data' => $prototipoExcluido,
+                ], 200);
+            } else {
+                return Response::json([
+                    'message' => 'Prototipo ou amostra não encontrado nas amostras!',
+                    'prototipo' => $datas['prototipo'],
+                    'amostra' => $datas['amostra'],
+                ], 404);
+            }
+        } catch (PDOException $e) {
+            return Response::json([
+                'message' => "Error: {$e->getMessage()}",
+                'line' => $e->getLine(),
+                'code' => $e->getCode()
+            ], 500);
+        } catch (Exception $e) {
+            return Response::json([
+                'message' => "Error: {$e->getMessage()}",
+                'line' => $e->getLine(),
+                'code' => $e->getCode()
+            ], 500);
+        }
+    }
+
 }
